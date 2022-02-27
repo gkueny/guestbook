247ca31378e481d6823b84ea2321a7c910ef3895
gkueny <gkueny.me@gmail.com>
2022-02-27 11:58:14 +0100

    Configure database sessions

---
+++ migrations/Version20220227105613.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220227105613 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create session table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+           CREATE TABLE sessions (
+               sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+               sess_data BYTEA NOT NULL,
+               sess_lifetime INTEGER NOT NULL,
+               sess_time INTEGER NOT NULL
+           )
+       ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE sessions');
+    }
+}
