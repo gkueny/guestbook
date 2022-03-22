25baa66fd107e32553e0bbcd7c30a53ec6641847
gkueny <gkueny.me@gmail.com>
2022-03-22 16:08:38 +0100

    Delete old rejected comments

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,11 +4,16 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 
 /**
  * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
@@ -19,10 +24,43 @@ use Doctrine\Persistence\ManagerRegistry;
 class CommentRepository extends ServiceEntityRepository
 {
     public const PAGINATOR_PER_PAGE = 2;
+    protected const DAYS_BEFORE_REJECTED_REMOVAL = 7;
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     */
+    public function countOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->select('COUNT(c.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :state_rejected or c.state = :state_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'state_rejected' => 'rejected',
+                'state_spam' => 'spam',
+                'date' => new DateTimeImmutable(-self::DAYS_BEFORE_REJECTED_REMOVAL . ' days'),
+            ]);
     }
 
     public function add(Comment $entity, bool $flush = true): void
